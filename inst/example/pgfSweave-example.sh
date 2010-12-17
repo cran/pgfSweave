03f5bb726ec515e0c4401c43ed8a6bbba13f56a3
Cameron Bracken <cameron.bracken@gmail.com>
2010-12-17 00:00:00 +0000

    version 1.1.2

--- inst/example/pgfSweave-example.sh
+++
@@ -1,3 +0,0 @@
-latex --jobname=pgfSweave-example-boxplot pgfSweave-example.tex
-dvipdf pgfSweave-example-boxplot.dvi
-pdftops -eps pgfSweave-example-boxplot.pdf
--- inst/misc/vignette-src/pgfSweave-vignette-source.sh
+++
@@ -1,2 +0,0 @@
-pdflatex --jobname=figs/fig-pgfSweave-hist pgfSweave-vignette-source.tex
-pdflatex --jobname=figs/fig-pgfSweave-tikz-hist pgfSweave-vignette-source.tex
